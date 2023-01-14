619fa8dc3be922e53ba3eb080dccf082d82f02b1
Francesca-Barcella <pollon.fra@gmail>
2023-01-14 15:52:29 +0100

    font & color

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -4,7 +4,7 @@
 
 <div class="container">
     @foreach($products as $type=>$list)
-    <h2 class="text-uppercase bg-secondary text-white p-2 mt-4">{{$type}}</h2>
+    <h2 class="text-uppercase bg-light text-black-50 bg-opacity-75 p-2 mt-4">{{$type}}</h2>
     <div class="row row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4">
         @foreach($list as $pasta)
         <div class="col">
